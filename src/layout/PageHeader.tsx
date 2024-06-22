203f7f545f9a83811cbdca28a38ed9d08164458a
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-06-22 13:14:54 +0530

    feat:header functions done

--- src/layout/PageHeader.tsx
+++ src/layout/PageHeader.tsx
@@ -1,38 +1,56 @@
 
+import { useState } from "react";
 import logo from "../assets/Logo.png"
 import Button from "../components/button";
-import {  Bell, Menu, Mic, Search, Upload, User2 } from "lucide-react";
+import { ArrowLeft, Bell, Menu, Mic, Search, Upload, User2 } from "lucide-react";
 
 const Header = () => {
+
+    const [showFullWidthSearch, setShowFullWidthSearch] = useState(false);
+    
     return <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
         {/* flex shrink 0 will stop shrinking this part */}
-        <div className="flex gap-4 items-center flex-shrink-0">
-            <Button variant="ghost" size={"icon"}> <Menu/> </Button>
-            <a href="/" className="h-6"> <img  className="h-6" src={logo} alt="" /></a>
+        <div className={`flex flex-shrink-0 items-center md:gap-2 ${showFullWidthSearch ? "hidden" : "flex"}`}>
+            <Button variant="ghost" size={"icon"}> <Menu /> </Button>
+            <a href="/" className="h-6"> <img className="h-6" src={logo} alt="" /></a>
         </div>
         {/* flex grow will grow the container with screen */}
-        <form className="flex gap-4 flex-grow justify-center">
+        <form className={`md:flex gap-4 flex-grow justify-center ${showFullWidthSearch ? 'flex' : 'hidden'}`}>
 
+            <Button onClick={()=>{setShowFullWidthSearch(false)}} className="md:hidden" size={"icon"} variant={"ghost"}>
+                <ArrowLeft />
+            </Button>
             <div>
-
-                <div className="flex flex-grow max-w-[600px]">
-                    <input/>
-                    <Button type="submit" size="default" variant="ghost"><Search/></Button>
+                <div className="flex flex-grow w- ">
+                    <input type="search" placeholder="Search" className="rounded-l-full border border-secondary-border shadow-secondary py-1 text-lg px-4 w-full
+                    focus:border-blue-500 outline-none"/>
+                    <Button
+                        className="py-2 px-4 rounded-r-full border border-1-0 flex-shrink-0 border-secondary-border shadow-secondary hover:bg-secondary-hover hover:text-white"
+                        type="submit" size="default" variant="ghost"><Search /></Button>
+                    <Button className="flex-shrink-0" type="button" variant={"ghost"} size={"icon"}><Mic /></Button>
                 </div>
-
             </div>
-            <Button className="flex-shrink-0" type="button" variant={"ghost"} size={"icon"}><Mic/></Button>
+
+
+
+
 
         </form>
-        <div className="flex flex-shrink-0 md:gap-2">
-            <Button size={"icon"} variant={"ghost"}>
-                <Upload/>
+        <div className={`flex flex-shrink-0 md:gap-2 ${showFullWidthSearch ? "hidden" : "flex"}`}>
+            <Button onClick={() => { setShowFullWidthSearch(true) }} className="md:hidden" size={"icon"} variant={"ghost"}>
+                <Search />
+            </Button>
+            <Button className="md:hidden" size={"icon"} variant={"ghost"}>
+                <Mic />
             </Button>
             <Button size={"icon"} variant={"ghost"}>
-                <Bell/>
+                <Upload />
             </Button>
             <Button size={"icon"} variant={"ghost"}>
-                <User2/>
+                <Bell />
+            </Button>
+            <Button size={"icon"} variant={"ghost"}>
+                <User2 />
             </Button>
         </div>
 
