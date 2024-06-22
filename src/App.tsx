57df63e6778a121744ea5f73b05e24708207723d
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2024-06-22 16:28:56 +0530

    feat: whole page grid added

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import './App.css'
 import Header from './layout/PageHeader'
 
 function App() {
-  
+
 
   return (
     <div className='flex flex-col max-h-screen'>
       <div className=''>
-        <Header/>
-        <span className='text-2xl'>dasdas</span>
+        <Header />
+        {/* flex grow 1 will let that part to grow the fill the entier screen */}
+        <div className='grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto'>
+          
+        </div>
       </div>
 
     </div>
